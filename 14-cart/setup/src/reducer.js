4430439d8347ae5c28d144e9cbf9b010a652fa02
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-24 15:22:36 +0100

    adding increase and decrease functionality

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,8 +1,34 @@
 const reducer = (state, action) => {
-    console.log(state);
+    
     if(action.type === 'CLEAR_CART') {
         return { ...state, cart:[] }
     }
+
+    if(action.type === 'REMOVE') {
+        const newCart = state.cart.filter((item) => item.id !== action.payload);
+        return {...state, cart: newCart};
+    }
+
+    if(action.type === 'INCREASE') {
+        let tempCart = state.cart.map((item) => {
+            if(item.id === action.payload) {
+                return { ...item, amount: item.amount + 1 }
+            }
+            return item;
+        }) 
+        return { ...state, cart: tempCart }
+    }
+
+    if(action.type === 'DECREASE') {
+        let tempCart = state.cart.map((item) => {
+            if(item.id === action.payload) {
+                return { ...item, amount: item.amount - 1 }
+            }
+            return item;
+        }).filter((item) => item.amount !== 0) 
+        return { ...state, cart: tempCart }
+    }
+
     return state;
 }
 
