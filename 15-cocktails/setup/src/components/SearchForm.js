d10d85e10c437512f44730a60e0eb9f9179067d4
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-26 13:10:07 +0100

    setting up SearchBar on CocktailsList

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -3,11 +3,29 @@ import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const serachValue = React.useRef('');
+
+  React.useEffect(() => {
+    serachValue.current.focus();
+  }, [])
+
+  const searchCocktail = () => {
+    setSearchTerm(serachValue.current.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
 
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favourite cocktail</label>
+          <input type='text' id='name' ref={serachValue} onChange={searchCocktail} />
+        </div>
+      </form>
+    </section>
   )
 }
 
